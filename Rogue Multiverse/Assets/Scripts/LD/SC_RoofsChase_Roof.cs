6c2e307abb2af22184d85569587b504a6fe99253
Benjamin <benjaminr@laposte.net>
2020-12-27 01:59:12 +0100

    Roofs working

--- Rogue Multiverse/Assets/Scripts/LD/SC_RoofsChase_Roof.cs
+++ Rogue Multiverse/Assets/Scripts/LD/SC_RoofsChase_Roof.cs
@@ -1,40 +1,65 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SC_RoofsChase_Roof : MonoBehaviour {
 
-    public Vector2 Size;
+    Vector2? prevSize;
+    public Vector2 size;
 
+    float? prevHeight;
     public float height;
 
-    private void OnValidate () {
+    List<SpriteRenderer> floors;
 
-        EditorApplication.delayCall += () => {
+    private void Start() {
 
-            if (this) {
+        UpdateVisuals();
 
-                transform.position = transform.position.Copy (null, null, -height);
+    }
 
-                GetComponent<SpriteRenderer> ().size = Size;
+    public void UpdateVisuals() {
 
-                GetComponent<BoxCollider2D> ().size = Size;                
+        if (gameObject.scene.IsValid() && prevSize == null || prevSize != size || prevHeight == null || prevHeight != height) {
 
-                Transform rightShadow = transform.GetChild (1);
+            prevSize = size;
 
-                rightShadow.localPosition = Size / 2;
+            prevHeight = height;
 
-                rightShadow.localScale = rightShadow.localScale.Copy (null, Size.y, null);
+            transform.position = transform.position.Copy(null, null, -height);
 
-                Transform bottomShadow = transform.GetChild (0);
+            GetComponent<SpriteRenderer>().size = size;
 
-                bottomShadow.localPosition = Size / -2;
+            GetComponent<BoxCollider2D>().size = size;
 
-                bottomShadow.localScale = new Vector3 (Size.x + rightShadow.localScale.x * .1f, height * 5, 1);
+            List<SpriteRenderer> f = new List<SpriteRenderer>(GetComponentsInChildren<SpriteRenderer>());
+
+            f.Remove(GetComponent<SpriteRenderer>());
+
+            foreach (SpriteRenderer sr in f)
+                DestroyImmediate(sr.gameObject);
+
+            for (int i = 0; i < height; i++) {
+
+                SpriteRenderer sr = new GameObject().AddComponent<SpriteRenderer>();
+
+                sr.transform.parent = transform;
+
+                sr.drawMode = SpriteDrawMode.Sliced;
+
+                sr.sprite = Resources.Load<Sprite>("Sprites/LD/RoofsChase/Building");
+
+                float w = sr.sprite.bounds.size.x - 1;
+
+                float h = sr.sprite.bounds.size.y - 1;
+
+                sr.transform.localPosition = new Vector3(w * (i + .5f), h * -(i + .5f), 2 * height);
+
+                sr.size = new Vector2(size.x + w, size.y + h);
 
             }
 
-        };
+        }
 
-    }
+    }  
 
 }
---
+++ Rogue Multiverse/Assets/Scripts/LD/SC_RoofsChase_Roof_Editor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(SC_RoofsChase_Roof))]
+[CanEditMultipleObjects]
+public class SC_RoofsChase_Roof_Editor : Editor {
+
+    public override void OnInspectorGUI() {
+
+        base.OnInspectorGUI();
+
+        foreach (SC_RoofsChase_Roof t in targets)
+                t.UpdateVisuals();
+
+    }
+
+}
