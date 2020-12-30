af7c8c6c8c7d6134a396c96d4b7429010784ef5f
Benjamin <benjaminr@laposte.net>
2020-12-30 23:59:01 +0100

    Custom editor script for Roofs to visualize their appearance out of game

---
+++ Rogue Multiverse/Assets/Scripts/LD/SC_RoofsChase_Roof_Editor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SC_RoofsChase_Roof))]
+[CanEditMultipleObjects]
+public class SC_RoofsChase_Roof_Editor : Editor {
+
+    public override void OnInspectorGUI() {
+
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Update visuals"))
+            foreach (SC_RoofsChase_Roof r in targets)
+                r.Setup();
+
+    }
+
+}
