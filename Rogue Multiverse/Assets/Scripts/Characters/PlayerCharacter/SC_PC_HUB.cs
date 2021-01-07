d28c41f5cd99bfe10a6f69dfa661f0574606f94e
Divi <benjaminr@laposte.net>
2021-01-07 21:05:20 +0100

    Only spacebar to jump in HUB

--- Rogue Multiverse/Assets/Scripts/Characters/PlayerCharacter/SC_PC_HUB.cs
+++ Rogue Multiverse/Assets/Scripts/Characters/PlayerCharacter/SC_PC_HUB.cs
@@ -24,7 +24,7 @@ public class SC_PC_HUB : SC_BasePlayerCharacter {
 
             if (!Grounded)
                 movement += Vector2.down * gravity * Time.deltaTime;
-            else if (Input.GetButtonDown("Vertical") || Input.GetButtonDown ("Jump")) {
+            else if (Input.GetButtonDown ("Jump")) {
 
                 jumpTime = 0;
 
@@ -42,7 +42,7 @@ public class SC_PC_HUB : SC_BasePlayerCharacter {
 
             movement += Vector2.up * (Mathf.Lerp (jumpStart, jumpStart + jumpHeight, lerp) - transform.position.y);
 
-            jumpTime = (lerp < 1) && (Input.GetButton ("Vertical") || Input.GetButton ("Jump")) ? jumpTime : -1;
+            jumpTime = (lerp < 1 && Input.GetButton ("Jump")) ? jumpTime : -1;
 
         }
 
