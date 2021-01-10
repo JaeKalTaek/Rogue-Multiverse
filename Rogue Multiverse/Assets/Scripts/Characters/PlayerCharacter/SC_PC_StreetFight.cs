e873bf930dcfaee728adab3aae51ec3fe42feafe
Divi <benjaminr@laposte.net>
2021-01-10 04:55:19 +0100

    Fixing SF's PC collision detection, SF's characters sprite order

--- Rogue Multiverse/Assets/Scripts/Characters/PlayerCharacter/SC_PC_StreetFight.cs
+++ Rogue Multiverse/Assets/Scripts/Characters/PlayerCharacter/SC_PC_StreetFight.cs
@@ -5,7 +5,7 @@ public class SC_PC_StreetFight : SC_BasePlayerCharacter {
     protected override Collider2D MovementCheck (Vector2 movement) {
 
         return Physics2D.BoxCast (transform.position + Vector3.up * .05f, new Vector2 (transform.lossyScale.x, .1f), 0, movement, movement.magnitude, LayerMask.GetMask ("Default")).collider ??
-            Physics2D.BoxCast (transform.position + Vector3.up * .25f, transform.lossyScale * .5f, 0, movement, movement.magnitude, LayerMask.GetMask ("Enemy")).collider;
+            Physics2D.BoxCast (transform.position + Vector3.up * transform.lossyScale.y * .25f, transform.lossyScale * .5f, 0, movement, movement.magnitude, LayerMask.GetMask ("Enemy")).collider;
 
     }
 
